b72008d37533e89c39259fbe3919d2b23c67c2d4
nyrogi <nozakimasanori853@gmail.com>
2021-10-14 18:22:57 +0900

    lesson4
    lesson4　配列

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,26 +7,13 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start() //void Start()に続く{}の中には、最初に一度だけ実行する処理
     {
-        int num = 100;
-        Debug.Log(num);
-        if (num <= 500)
+        int[] array = new int[10];//配列arrayを10で初期化
+
+        for (int i = 0; i < array.Length; i++)
         {
-            // 要素数5の配列を初期化する
-            int[] points = new int[5];
-
-            // 配列の各要素に値を代入する
-            points[0] = 30;
-            points[1] = 20;
-            points[2] = 50;
-            points[3] = 10;
-            points[4] = 80;
-
-            // 配列の要素をすべて表示する
-            for (int i = 0; i < 5; i++)
-            {
-                Debug.Log(points[i]);
-            }
+            Debug
         }
+
     }
     
 
