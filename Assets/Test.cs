f98e62860940a2ce8dff97a681c87d4442f6837e
nyrogi <nozakimasanori853@gmail.com>
2021-11-04 18:04:59 +0900

    完成
    課題完了

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start() //void Start()に続く{}の中には、最初に一度だけ実行する処理
+    
     {
-        int[] array = new int[10];//配列arrayを10で初期化
+        int[] array = new int[5];//配列arrayを5で初期化
 
-        for (int i = 0; i < array.Length; i++)
+        for (int i = 0; i < array.Length; i++)//逆順
         {
-            Debug
+            Debug.Log(5 - i);
+        }
+        for (int i = 0; i < array.Length; i++)//順番
+        {
+
+            Debug.Log(i);
+        }
+        int mp = 53;
+
+        //10回繰り返す
+        for (int i = 0; i < 10; i++)
+        {
+            Magic();
         }
 
+        void Magic()//Magic関数宣言
+        {
+
+            if (mp >= 5)
+            {
+                int num = 5;
+                //mpが5以上の時5減らす
+                Debug.Log(mp - num);
+                Debug.Log("魔法攻撃をした残りmpは"+mp);
+                num = num + 5;
+            }
+            else
+            {
+                Debug.Log("mpが足りないため、魔法が使えない");
+            }
+
+        }
+        
+            
     }
     
 
